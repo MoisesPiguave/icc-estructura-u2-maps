926c4db6a932cdb8229ff89c60afc838d3b312c3
MoisesPiguave <moi.emanuelps@gmail.com>
2025-07-09 16:06:26 +0100

    tarea

--- src/controllers/EmpleadoDAOTreemap.java
+++ src/controllers/EmpleadoDAOTreemap.java
@@ -2,42 +2,34 @@ package controllers;
 
 import java.util.Map;
 import java.util.TreeMap;
-
-
 import models.Empleado;
 
 public class EmpleadoDAOTreemap implements EmpleadoDAO {
-
-    private Map<Empleado, Empleado> empleados;
+    private Map<Integer, Empleado> empleados;
 
     public EmpleadoDAOTreemap() {
-        empleados = new TreeMap<>(); 
+        empleados = new TreeMap<>();  // Usamos un TreeMap con el ID del empleado como clave
     }
 
     @Override
     public void add(Empleado empleado) {
-        empleados.put(empleado, empleado);
+        empleados.put(empleado.getId(), empleado);  // Usamos el ID como clave
     }
 
     @Override
     public void remove(int id) {
-       Empleado emp = new Empleado(id);
-        empleados.remove(emp);
+        empleados.remove(id);  // Removemos un empleado por ID
     }
 
     @Override
     public void list() {
-    if (empleados.isEmpty()) {
-        System.out.println("No hay empleados registrados.");
-    } else {
-        System.out.println("Lista de empleados:");
-        for (Empleado empleado : empleados.values()) {
-            System.out.println(empleado);
+        if (empleados.isEmpty()) {
+            System.out.println("No hay empleados registrados.");
+        } else {
+            System.out.println("Lista de empleados ordenados por ID:");
+            for (Empleado empleado : empleados.values()) {  // Iteramos sobre los valores del mapa
+                System.out.println(empleado);  // Imprime cada empleado
+            }
         }
     }
-
-}
-
-        
-
 }
